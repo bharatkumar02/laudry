d17b2d55aa165867b44b44f6c7f414f70467b9b6
bharatkumar02 <kumarbharat9416@gmail.com>
2024-03-07 23:11:30 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('images/cloths.jpg')",
+    }
+    },
+  },
+  plugins: [],
+}
+
